cd4027432fea0322f6666bb780e9e6a469451698
Fakiros <losfakiros@gmail.com>
2018-12-16 13:11:14 +0100

    Aj zo zalohou MAIN

--- main.c
+++ main.c
@@ -1,16 +1,79 @@
 #ifndef __PROGTEST__
+
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <ctype.h>
 #include <assert.h>
+
 #endif /* __PROGTEST__ */
 
-char             * wordWrap                                ( int               width,
-                                                             const char      * src )
-{
-    /* todo */
+char *wordWrap(int width, const char *src) {
+    char *vystup, *tmpVystup, *slovo, *tmpSlovo;
+    char *tmpSrc;
+    int odsek = 0, moznoOdsek = 0;  /*default FALSE*/
+    int dlzkaRiadku = 0, dlzkaSlova, i;
 
+    if (strlen(src) == 0)   /*prazdny retazec*/
+        return calloc(1, sizeof(char));
+    /*init*/
+    tmpSrc = (char *) src;
+    vystup = (char *) calloc(strlen(src) + 1, sizeof(char)); /*pre istotu +1, ak by vstup nemal \n*/
+    tmpVystup = vystup;
+    slovo = (char *) calloc((size_t) width + 1, sizeof(char)); /*jedno slovo nemoze byt dlhsie ako riadok*/
+    tmpSlovo = slovo;   /*nastavim smernik na zaciatok*/
+    dlzkaSlova = 0;
+
+    do {
+        if (dlzkaSlova > 0 && (*tmpSrc == ' ' || *tmpSrc == '\t' || *tmpSrc == '\n' || *tmpSrc == 0)) {
+            /*koniec slova, pridat na koniec riadku*/
+            if (dlzkaRiadku > 0) {
+                *tmpVystup++ = ' ';
+                dlzkaRiadku++;
+            }
+            tmpSlovo = slovo;
+            for (i = 0; i < dlzkaSlova; ++i)   /*pridam slovo na koniec */
+                *tmpVystup++ = *tmpSlovo++;
+            dlzkaRiadku += dlzkaSlova;
+            dlzkaSlova = 0;
+            tmpSlovo = slovo; /*nastavim smernik na zaciatok*/
+            if (*tmpSrc == '\n')
+                moznoOdsek = 1;
+
+        } else if (*tmpSrc != ' ' && *tmpSrc != '\t' && *tmpSrc != '\n') {
+            /*pridavam pismekna slova*/
+            if (moznoOdsek) moznoOdsek = 0;
+            if (odsek) {
+                *tmpVystup++ = '\n'; /*treba zariadkovat vystup 2x */
+                *tmpVystup++ = '\n';
+                dlzkaRiadku = 0;
+                odsek = 0;
+            }
+            if (++dlzkaSlova > width) {     /*koniec s chybou*/
+                free(vystup);
+                free(slovo);
+                return NULL;
+            }
+            *tmpSlovo++ = *tmpSrc;
+
+            if (1 + dlzkaSlova + dlzkaRiadku > width) {
+                *tmpVystup++ = '\n'; /*treba zariadkovat vystup*/
+                dlzkaRiadku = 0;
+            }
+
+        } else if (moznoOdsek && *tmpSrc == '\n') {
+            odsek = 1;  /*odsek*/
+
+        } else {
+            if (*tmpSrc == '\n') moznoOdsek = 1; else moznoOdsek = 0;
+        }
+
+    } while (*tmpSrc++ != 0);    /*kym nieje koniec*/
+    if (dlzkaRiadku > 0)
+        *tmpVystup++ = '\n'; /*treba zariadkovat vystup*/
+
+    free(slovo);
+    return vystup;
 }
 
 #ifndef __PROGTEST__
@@ -57,6 +120,8 @@ int                main                                     ( void )
             "Fusce    faucibus, turpis nec auctor posuere, nulla tellus scelerisque metus,\n"
             "quis molestie mi dui id quam. Mauris vestibulum. Nam ullamcorper.\n"
             "\n";
+    printf("\n glupocina\n");
+
     res = wordWrap ( 40, s0 );
     assert ( ! strcmp ( res,
                         "Lorem ipsum dolor sit amet, consectetuer\n"
